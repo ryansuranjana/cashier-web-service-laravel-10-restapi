d6248b73fe7b1777d433924981afbbef332bf5d3
ryansuranjana <ryansuranjana23@gmail.com>
2023-05-22 21:31:57 +0800

    make payment collection and resource

---
+++ app/Http/Resources/Payment/PaymentResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Payment;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PaymentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'type' => $this->type,
+            'logo' => $this->logo
+        ];
+    }
+}
