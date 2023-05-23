c7b7b2c9f27cd2c28c9e1f52b0844650b48adc49
ryansuranjana <ryansuranjana23@gmail.com>
2023-05-23 13:18:07 +0800

    fix relation order payment

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -24,6 +24,6 @@ class Order extends Model
 
     public function payment()
     {
-        return $this->belongsTo(Payment::class);
+        return $this->belongsTo(Payment::class, 'payment_type_id');
     }
 }
